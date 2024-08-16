087a218e82c59090fa601ddd4bc1bb0f4c080226
KrlsRomero <car_683@outlook.com>
2024-08-16 14:11:53 -0500

    Actualización sistema

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'app-tiendaOnline';
+
+  ngOnInit(): void {
+    initFlowbite();
+
+    document.documentElement.classList.add('dark');
+
+  }
+}
